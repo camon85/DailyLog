4e5f9ff33174802263bf6e8b62f42203ee495fe6
Jooyong Sung <camon85@gmail.com>
2015-11-02 19:17:09 +0900

    Initial commit

---
+++ src/main/java/com/camon/dailylog/HelloController.java
@@ -0,0 +1,24 @@
+package com.camon.dailylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jooyong on 2015-11-02.
+ */
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+}
