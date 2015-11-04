eae40fbca0d3cb534399c00796a578f3bd0c2ec4
Jooyong Sung <camon85@gmail.com>
2015-11-04 14:21:33 +0900

    Test BCrypt

---
+++ src/test/java/BCryptTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import org.mindrot.jbcrypt.BCrypt;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by jooyong on 2015-11-04.
+ */
+public class BCryptTest {
+
+    @Test
+    public void test(){
+        String password = "암호";
+        String password2= "암호2";
+
+        System.out.println("plan text : "+ password);
+
+        String hashPass = BCrypt.hashpw(password, BCrypt.gensalt(12));
+        System.out.println("password hash : " + hashPass);
+
+        assertTrue(BCrypt.checkpw(password, hashPass));
+        assertFalse(BCrypt.checkpw(password2, hashPass));
+    }
+}
